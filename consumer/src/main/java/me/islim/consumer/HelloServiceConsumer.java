af6a767e2d1a4bbcd4e73458fa180043c00504d6
liuxiaozhu <xiaozhu.liu1987@gmail.com>
2017-11-21 16:53:13 +0800

    异步请求

--- consumer/src/main/java/me/islim/consumer/HelloServiceConsumer.java
+++ consumer/src/main/java/me/islim/consumer/HelloServiceConsumer.java
@@ -1,15 +1,20 @@
 package me.islim.consumer;
 
+import com.alibaba.dubbo.rpc.RpcContext;
 import me.islim.service.HelloService;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
+
 public class HelloServiceConsumer {
 
-    public static void main(String[] args){
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
         ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
         HelloService service = context.getBean(HelloService.class);
-        System.out.println(service.sayHello("lxz"));
-
+        Future<String> f = RpcContext.getContext().asyncCall(() -> service.sayHello("lxz"));
+        System.out.println(f.get());
     }
 }
